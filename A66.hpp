64e4872ade0e1601ccb05d040a767544c88f969a
Carolyn <carol.nguyen2007@gmail.com>
2022-10-11 00:04:38 -0700

    hi

--- A66.hpp
+++ A66.hpp
@@ -11,8 +11,8 @@ int maxmin(int num1, int num2, int num3, int &max, int &min)
     int nums[3]; 
     nums[0] = num1, nums[1] = num2, nums[2] = num3;
 
-    max = num1;
-    min = num1; 
+    max = num1; // set max to num1
+    min = num1; // set min to num1
     for (int i = 1; i < 3; i++)
     {
         if (nums[i] > max)
