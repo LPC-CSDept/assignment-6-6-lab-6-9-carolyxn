75f9fa8de07f746b3b37a3243f222b86ecda2c29
Carolyn <carol.nguyen2007@gmail.com>
2022-10-10 23:59:01 -0700

    again

--- A66.hpp
+++ A66.hpp
@@ -5,3 +5,19 @@
 using namespace std;
 
 // complete the function maxmin( )
+
+int maxmin(int num1, int num2, int num3, int &max, int &min)
+{
+    int nums[3]; 
+    nums[0] = num1, nums[1] = num2, nums[2] = num3;
+
+    max = num1;
+    min = num1; 
+    for (int i = 1; i < 3; i++)
+    {
+        if (nums[i] > max)
+            max = nums[i];
+        if (nums[i] < min)
+            min = nums[i];
+    }
+}
